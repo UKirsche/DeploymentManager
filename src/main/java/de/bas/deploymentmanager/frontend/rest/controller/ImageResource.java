96b6c0b2356addfe4471a3decdd60dbf0b7689e9
stephanlubitz <Stephan_Lubitz@me.com>
2020-04-05 14:00:35 +0200

    Beim Starten der Pipeline ein vorläufiges Tag erzeugen

--- src/main/java/de/bas/deploymentmanager/frontend/rest/controller/ImageResource.java
+++ src/main/java/de/bas/deploymentmanager/frontend/rest/controller/ImageResource.java
@@ -1,7 +1,6 @@
 package de.bas.deploymentmanager.frontend.rest.controller;
 
 import de.bas.deploymentmanager.logic.domain.project.boundary.ProjectService;
-import de.bas.deploymentmanager.logic.domain.project.entity.Image;
 import de.bas.deploymentmanager.logic.domain.project.entity.NewImageModel;
 import de.bas.deploymentmanager.logic.domain.project.entity.Project;
 import org.slf4j.Logger;
@@ -10,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -21,10 +19,6 @@ public class ImageResource {
     @Inject
     private ProjectService projectService;
 
-    @GET
-    public List<Image> getAllImages(@PathParam("project") String application) {
-        return projectService.getImages(application);
-    }
 
     /**
      * Erzeugt ein neues Image und gibt das Tag wieder zurück.
@@ -48,6 +42,7 @@ public class ImageResource {
      */
     @GET
     public String getNextTag(@PathParam("project") String identifier, @QueryParam("version") String version) {
+        log.info("Generate Next Tag für Project {} und Version {}", identifier, version);
         return projectService.gererateNextTag(identifier, version);
     }
 
