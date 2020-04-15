7596c96f482b56437c08c388de26318b458e24b1
Stephan Lubitz <Stephan.Lubitz@bas.bund.de>
2020-04-15 21:31:18 +0200

    HostService und Impl gelöscht. Diese sollen nicht nach außen gegeben weren. Wird eine Information über einen Host benötigt dann im StageService implementieren

--- src/main/java/de/bas/deploymentmanager/logic/domain/stage/boundary/HostService.java
+++
@@ -1,8 +0,0 @@
-package de.bas.deploymentmanager.logic.domain.stage.boundary;
-
-import de.bas.deploymentmanager.logic.domain.stage.entity.Host;
-
-public interface HostService {
-
-    Host getHost(Long id);
-}
